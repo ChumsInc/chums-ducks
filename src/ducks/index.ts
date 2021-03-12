d86ef7fde5890f565b2483231ffd5969ffa2b3f6
Steve Montgomery <steve@chums.com>
2021-03-12 16:24:04 -0700

    Initial commit

---
+++ src/ducks/index.ts
@@ -0,0 +1,16 @@
+import {combineReducers} from 'redux';
+
+import alertsReducer from './alerts';
+// import ordersReducer from './orders';
+// import pageReducer from './page';
+// import customersReducer from './customers';
+
+const rootReducer = combineReducers({
+    alerts: alertsReducer,
+    // orders: ordersReducer,
+    // page: pageReducer,
+    // customers: customersReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>
+export default rootReducer;
