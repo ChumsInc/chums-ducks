6403050cbac4d9a42e743b05383f6739e78ea187
Steve Montgomery <steve@chums.com>
2021-03-16 09:25:26 -0600

    Test 3

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-export * as AlertDuck from './ducks/alerts';
+export { default as alertReducer, onErrorAction, addAlertAction, alertSelector, dismissAlertAction } from './ducks/alerts';
 export * as Alert from './ducks/alerts/Alert';
 export * as AlertList from './ducks/alerts/AlertList';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * as AlertDuck from './ducks/alerts';
+export {default as alertReducer, onErrorAction, addAlertAction, alertSelector, dismissAlertAction}  from './ducks/alerts';
 export * as Alert from './ducks/alerts/Alert';
 export * as AlertList from './ducks/alerts/AlertList';
