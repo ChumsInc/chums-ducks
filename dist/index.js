6d0f944d53e3da58ca30e127f14101eb19d94921
Steve Montgomery <steve@chums.com>
2021-04-16 15:05:47 -0600

    Added Pagination, new options

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,6 @@ export { default as alertReducer, onErrorAction, addAlertAction, selectAlertList
 export { default as Alert } from './ducks/alerts/Alert';
 export { default as AlertList } from './ducks/alerts/AlertList';
 export { default as Badge } from './components/Badge';
+export { default as pageReducer } from './ducks/page';
+export * from './ducks/page';
 //# sourceMappingURL=index.js.map
