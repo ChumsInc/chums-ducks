59c5411971e090a05cbbc089905d215353a85800
Steve Montgomery <steve@chums.com>
2021-08-16 10:00:47 -0600

    Add icon to nav items

--- dist/ducks/tabs/NavRouterLink.js
+++ dist/ducks/tabs/NavRouterLink.js
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import { NavLink } from 'react-router-dom';
 var NavRouterLink = function (_a) {
-    var to = _a.to, id = _a.id, title = _a.title, canClose = _a.canClose, disabled = _a.disabled, className = _a.className, onClose = _a.onClose;
+    var to = _a.to, id = _a.id, title = _a.title, icon = _a.icon, canClose = _a.canClose, disabled = _a.disabled, className = _a.className, onClose = _a.onClose;
     var onClickClose = function (ev) {
         ev.preventDefault();
         ev.stopPropagation();
@@ -12,6 +12,7 @@ var NavRouterLink = function (_a) {
     };
     return (React.createElement("li", { className: "nav-item" },
         React.createElement(NavLink, { to: to, activeClassName: "active", className: classNames('nav-link', className, { disabled: disabled }), tabIndex: disabled ? -1 : 0 },
+            !!icon && React.createElement("span", { className: classNames('nav-item-icon', icon) }),
             title,
             canClose && (React.createElement("span", { "aria-label": "Close", onClick: onClickClose, className: "ms-2 bi-x-lg" })))));
 };
--- src/ducks/tabs/NavRouterLink.tsx
+++ src/ducks/tabs/NavRouterLink.tsx
@@ -5,20 +5,21 @@ import {NavLink} from 'react-router-dom';
 
 
 export interface NavRouterLinkProps extends Tab {
-    to:string|object|((any:any) => string|object),
+    to: string | object | ((any: any) => string | object),
     className?: string | object,
     onClose?: (id?: string) => void,
 }
 
 const NavRouterLink: React.FC<NavRouterLinkProps> = ({
-                                             to,
-                                             id,
-                                             title,
-                                             canClose,
-                                             disabled,
-                                             className,
-                                             onClose
-                                         }) => {
+                                                         to,
+                                                         id,
+                                                         title,
+                                                         icon,
+                                                         canClose,
+                                                         disabled,
+                                                         className,
+                                                         onClose
+                                                     }) => {
     const onClickClose = (ev: MouseEvent) => {
         ev.preventDefault();
         ev.stopPropagation();
@@ -30,7 +31,8 @@ const NavRouterLink: React.FC<NavRouterLinkProps> = ({
     return (
         <li className="nav-item">
             <NavLink to={to} activeClassName="active"
-                     className={classNames('nav-link', className, {disabled})} tabIndex={disabled ? -1 : 0} >
+                     className={classNames('nav-link', className, {disabled})} tabIndex={disabled ? -1 : 0}>
+                {!!icon && <span className={classNames('nav-item-icon', icon)}/>}
                 {title}
                 {canClose && (
                     <span aria-label="Close" onClick={onClickClose} className="ms-2 bi-x-lg"/>
